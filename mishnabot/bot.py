48a53a350782d696ffa45bb9e2919e4154106b24
Seth Alter <alters@mit.edu>
2022-03-27 18:53:09 -0400

    Don't encapsulate everything in try/finally.

--- mishnabot/bot.py
+++ mishnabot/bot.py
@@ -35,33 +35,34 @@ class Bot(discord.Client):
         self.log(f"{datetime.today()}: BOT IS ALIVE!!")
         # Connect to the Discord channel.
         channel = self.get_channel(self.channel)
-        try:
-            while True:
-                today = datetime.today()
-                # Don't post on Shabbos.
-                if self.shomer and today.isoweekday() == 5:
-                    self.log(f"{today}: Skipping because it's Shabbos.")
-                else:
-                    # Get a random sugye.
-                    sugye = self.mishnah[self.rng.randint(0, len(self.mishnah) + 1)]
-                    url = f"https://www.sefaria.org/Mishnah_{sugye['Order'].replace(' ', '_')}.{sugye['Chapter']}.{sugye['Verse']}?lang=bi"
-                    # Get the English text. Convert HTML tags to markdown tags.
-                    en = sugye['en'].replace("<b>", "**").replace("</b>", "**").replace("<i>", "_").replace("</i>", "_")
-                    # Create a citation.
-                    citation = f"{sugye['Order']} {sugye['Chapter']}.{sugye['Verse']}"
-                    self.log(f"{today}: {citation}")
-                    # Get the text.
-                    he = sugye["he"]
-                    text = f"**{citation}**\n{he}\n{en}\n{url}"
-                    # Split the text into posts of <= 2000 characters.
-                    posts = [text[index: index + 2000] for index in range(0, len(text), 2000)]
+        while True:
+            today = datetime.today()
+            # Don't post on Shabbos.
+            if self.shomer and today.isoweekday() == 5:
+                self.log(f"{today}: Skipping because it's Shabbos.")
+            else:
+                # Get a random sugye.
+                sugye = self.mishnah[self.rng.randint(0, len(self.mishnah) + 1)]
+                url = f"https://www.sefaria.org/Mishnah_{sugye['Order'].replace(' ', '_')}.{sugye['Chapter']}.{sugye['Verse']}?lang=bi"
+                # Get the English text. Convert HTML tags to markdown tags.
+                en = sugye['en'].replace("<b>", "**").replace("</b>", "**").replace("<i>", "_").replace("</i>", "_")
+                # Create a citation.
+                citation = f"{sugye['Order']} {sugye['Chapter']}.{sugye['Verse']}"
+                self.log(f"{today}: {citation}")
+                # Get the text.
+                he = sugye["he"]
+                text = f"**{citation}**\n{he}\n{en}\n{url}"
+                # Split the text into posts of <= 2000 characters.
+                posts = [text[index: index + 2000] for index in range(0, len(text), 2000)]
+                try:
                     # Post.
                     for post in posts:
                         await channel.send(post)
-                # Wait a day.
-                sleep(86400)
-        finally:
-            self.log(f"{datetime.today()} BOT IS DOWN!!")
+                except Exception as e:
+                    self.log(str(e))
+                    exit()
+            # Wait a day.
+            sleep(86400)
 
     def log(self, message: str) -> None:
         """
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mishnabot',
-    version="1.0.0",
+    version="1.0.1",
     description='Discord bot that posts a random sugye per day.',
     long_description='Discord bot that posts a random sugye per day.',
     long_description_content_type='text/markdown',
